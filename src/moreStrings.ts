df8efdf3ed9261805c10d71de837444ffbcf6ed8
Patrick Roza <contact@patrickroza.com>
2023-12-25 12:40:05 +0100

    fml

--- src/moreStrings.ts
+++ src/moreStrings.ts
@@ -6,6 +6,13 @@ import { type B, S } from "./schema.js"
 import { NonEmptyString } from "./strings.js"
 import type { NonEmptyString255Brand, NonEmptyString2k, NonEmptyStringBrand } from "./strings.js"
 import type { Refinement } from "effect/Predicate"
+import { nanoid } from "nanoid"
+
+export const extendM = <T extends {}, X extends {}>(a: T, ext: (a: T) => X) => {
+  Object.assign(a, ext(a))
+  return a as typeof a & X
+}
+
 
 /**
  * A string that is at least 1 character long and a maximum of 50.
@@ -46,6 +53,116 @@ export const Min3String255 = pipe(
   fromBrand(nominal<NonEmptyString2k>())
 )
 
+/**
+ * A string that is at least 6 characters long and a maximum of 50.
+ */
+export interface StringIdBrand extends Simplify<B.Brand<"StringId"> & NonEmptyString50Brand> { }
+
+/**
+ * A string that is at least 6 characters long and a maximum of 50.
+ */
+export type StringId = string & StringIdBrand
+
+const makeStringId = (): StringId => nanoid() as unknown as StringId
+const minLength = 6
+const maxLength = 50
+const StringIdArb = (): Arbitrary<string> => (fc) =>
+  fc
+    //.uint8Array({ minLength, maxLength })
+    .string({ minLength, maxLength })
+//.map((_) => customRandom(urlAlphabet, maxLength, (size) => _.subarray(0, size))())
+
+/**
+ * A string that is at least 6 characters long and a maximum of 50.
+ */
+export const StringId = extendM(
+  pipe(
+    S.string,
+    S.minLength(minLength),
+    S.maxLength(maxLength, { title: "StringId", arbitrary: StringIdArb }),
+    fromBrand(nominal<StringIdBrand>())
+  ),
+  (s) => ({
+    make: makeStringId,
+    withDefault: () => S.withDefaultConstructor(s, makeStringId)
+  })
+)
+
+// const prefixedStringIdUnsafe = (prefix: string) => StringId(prefix + StringId.make())
+
+// const prefixedStringIdUnsafeThunk = (prefix: string) => () => prefixedStringIdUnsafe(prefix)
+
+export function prefixedStringId<Brand extends StringId>() {
+  return <Prefix extends string, Separator extends string = "-">(
+    prefix: Prefix,
+    name: string,
+    separator?: Separator
+  ) => {
+    type FullPrefix = `${Prefix}${Separator}`
+    // type PrefixedId = `${FullPrefix}${string}`
+
+    const pref = `${prefix}${separator ?? "-"}` as FullPrefix
+    // const fromString = pipe(
+    //   stringIdFromString,
+    //   refine(
+    //     refinement,
+    //     (n) => leafE(customE(n, `a StringId prefixed with '${pref}'`))
+    //   ),
+    //   arbitrary((FC) =>
+    //     stringIdArb(FC).map(
+    //       (x) => (pref + x.substring(0, 50 - pref.length)) as Brand
+    //     )
+    //   )
+    // )
+    const arb = (): Arbitrary<string> => (fc) =>
+      StringIdArb()(fc).map(
+        (x) => (pref + x.substring(0, 50 - pref.length)) as Brand
+      )
+    const schema = StringId.pipe(
+      S.filter((x: StringId): x is Brand => x.startsWith(pref), { arbitrary: arb, title: name })
+    )
+    const make = () => (pref + StringId.make().substring(0, 50 - pref.length)) as Brand
+
+    return extendM(
+      schema,
+      (ex): PrefixedStringUtils<Brand, Prefix, Separator> => ({
+        make,
+        /**
+         * Automatically adds the prefix.
+         */
+        unsafeFrom: (str: string) => ex(pref + str),
+        /**
+         * Must provide a literal string starting with prefix.
+         */
+        prefixSafe: <REST extends string>(str: `${Prefix}${Separator}${REST}`) => ex(str),
+        prefix,
+        withDefault: () => S.withDefaultConstructor(schema, make)
+      })
+    )
+  }
+}
+
+// export const brandedStringId = <Brand extends StringId>() =>
+//   extendM(StringId.pipe(S.brand<Brand>()), (s) => {
+//     const make = (): Brand => StringId.make() as unknown as Brand
+
+//     return ({
+//       make,
+//       withDefault: S.optional(s, { default: make })
+//     })
+//   })
+
+export interface PrefixedStringUtils<
+  Brand extends StringId,
+  Prefix extends string,
+  Separator extends string
+> {
+  readonly make: () => Brand
+  readonly unsafeFrom: (str: string) => Brand
+  prefixSafe: <REST extends string>(str: `${Prefix}${Separator}${REST}`) => Brand
+  readonly prefix: Prefix
+  readonly withDefault: () => S.ConstructorPropertyDescriptor<string, Brand>
+}
 
 export interface UrlBrand extends Simplify<B.Brand<"Url"> & NonEmptyStringBrand> { }
 
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,5 @@
+
+import { StringId } from "./moreStrings.js"
 import { S } from "./schema.js"
 import { NonEmptyString, NonEmptyString255 } from "./strings.js"
 
@@ -11,7 +13,10 @@ const a3 = S.string.parse("abc")
 
 const C3 = S.string.Arbitrary.sample
 
+const C4 = StringId.make()
+class C5 extends S.Class<C5>()({ a: StringId, b: StringId.withDefault() }) { }
+
 console.log({
   a, b, c, a2, b2, a3
 })
-console.log(C3)
+console.log({ C3, C4, C5: new C5({ a: "abcdef" }) })
